de857a133b2fa2f03d6b9c9c797595686cc39590
mbettinger <49953678+mbettinger@users.noreply.github.com>
2024-08-07 14:25:38 +0200

    Replaced delay attack's negative IP filter with a positive filter, to only target honest requesters

--- deployed/out/mal_datt_config.sh
+++ deployed/out/mal_datt_config.sh
@@ -1,4 +1,5 @@
-COOLTEE_MAL_REQ_SUBNET="10.0.4.0/24"
+#!/bin/sh
+COOLTEE_HON_REQ_SUBNET="10.0.3.0/24"
 ATTACK_DELAY="50ms"
 
 RED='\033[0;31m'
@@ -10,12 +11,12 @@ for arg
 do
     case $arg in
         --help|-h)
-            echo -e "${PINK}Usage: $0 [{--help|-h} | [--subnet=<IP_MASK>] [--delay=<DELAY>] | --rm]${NC}"
-            echo -e "  --help, -h          Display this help message."
-            echo -e "  --subnet=<IP_MASK>  The mask of the subnet to which the malicious delay attack should be applied. Default: ${COOLTEE_MAL_REQ_SUBNET}"
-            echo -e "  --delay=<DELAY>     The delay to be applied to the packets. Default: ${ATTACK_DELAY}"
-            echo -e "  --rm                Remove a previous malicious delay attack configuration."
-            echo -e "  <no arguments>      Apply the malicious delay attack with the default configuration."
+            echo "${PINK}Usage: $0 [{--help|-h} | [{-hs|--hon-subnet}=<IP_MASK>] [{-ms|--mal-subnet}=<IP_MASK>] [--delay=<DELAY>] | --rm]${NC}"
+            echo "  --help, -h          Display this help message."
+            echo "  {-s|--subnet}=<IP_MASK>  The mask of the honest requester's subnet to which the malicious delay attack should be applied. Default: ${COOLTEE_HON_REQ_SUBNET}"
+            echo "  {-d|--delay}=<DELAY>     The delay to be applied to the packets. Default: ${ATTACK_DELAY}"
+            echo "  --rm                Remove a previous malicious delay attack configuration."
+            echo "  <no arguments>      Apply the malicious delay attack with the default configuration."
             exit 0
     esac
 done
@@ -24,13 +25,26 @@ for arg
 do
     case $arg in
         --rm)
-            echo -e "${PINK}Disabling the malicious delay attack${NC}"
-            sudo iptables -t mangle -F MARK_PACKETS
-            sudo iptables -t mangle -D OUTPUT -j MARK_PACKETS
-            sudo iptables -t mangle -X MARK_PACKETS
+            echo "${PINK}Disabling the malicious delay attack${NC}"
+            if sudo iptables --table mangle --check OUTPUT -j MARK_PACKETS > /dev/null 2>&1
+            then
+                echo "${YEL}Disabling found iptables configuration.${NC}"
+                sudo iptables -t mangle -F MARK_PACKETS
+                sudo iptables -t mangle -D OUTPUT -j MARK_PACKETS
+                sudo iptables -t mangle -X MARK_PACKETS
+                echo "${YEL}Disabling found iptables configuration.${NC}"
+            else
+                echo "${YEL}No iptables configuration found.${NC}"
+            fi
 
-            sudo tc qdisc del dev eth0 root
-            echo -e "${PINK}Disabled the malicious delay attack${NC}"
+            if sudo tc qdisc show dev eth0 | grep -q "qdisc netem 10:" > /dev/null 2>&1
+            then
+                echo "${YEL}Disabling found tc configuration.${NC}"
+                sudo tc qdisc del dev eth0 root
+                echo "${YEL}Disabled found tc configuration.${NC}"
+            else
+                echo "${YEL}No tc configuration found.${NC}"
+            fi
             exit 0
             ;;
     esac
@@ -39,43 +53,42 @@ done
 for arg
 do
     case $arg in
-        --subnet=*)
-            COOLTEE_MAL_REQ_SUBNET="${arg#*=}"
+        --subnet=*|-s=*)
+            COOLTEE_HON_REQ_SUBNET="${arg#*=}"
             ;;
         --delay=*)
             ATTACK_DELAY="${arg#*=}"
             ;;
     esac
 done
-echo -e "${PINK}Malicious delay attack configuration:${NC}"
-echo -e "COOLTEE_MAL_REQ_SUBNET: $COOLTEE_MAL_REQ_SUBNET"
-echo -e "ATTACK_DELAY: $ATTACK_DELAY"
+echo "${PINK}Malicious delay attack configuration:${NC}"
+echo "COOLTEE_HON_REQ_SUBNET: $COOLTEE_HON_REQ_SUBNET"
+echo "ATTACK_DELAY: $ATTACK_DELAY"
 
-echo -e "${PINK}Checking previous configuration.${NC}"
+echo "${PINK}Checking previous configuration.${NC}"
 if sudo iptables --table mangle --check OUTPUT -j MARK_PACKETS > /dev/null 2>&1
 then
-    echo -e "${YEL}Previous iptables configuration found. Removing it.${NC}"
+    echo "${YEL}Previous iptables configuration found. Removing it.${NC}"
     sudo iptables -t mangle -F MARK_PACKETS
     sudo iptables -t mangle -D OUTPUT -j MARK_PACKETS
     sudo iptables -t mangle -X MARK_PACKETS
-    echo -e "${YEL}Previous iptables configuration removed.${NC}"
+    echo "${YEL}Previous iptables configuration removed.${NC}"
 fi
 
 if sudo tc qdisc show dev eth0 | grep -q "qdisc netem 10:" > /dev/null 2>&1
 then
-    echo -e "${YEL}Previous tc configuration found. Removing it.${NC}"
+    echo "${YEL}Previous tc configuration found. Removing it.${NC}"
     sudo tc qdisc del dev eth0 root
-    echo -e "${YEL}Previous tc configuration removed.${NC}"
+    echo "${YEL}Previous tc configuration removed.${NC}"
 fi
 
-echo -e "${PINK}Enabling the malicious delay attack.${NC}"
+echo "${PINK}Enabling the malicious delay attack.${NC}"
 
 sudo iptables -t mangle -N MARK_PACKETS
 sudo iptables -t mangle -A OUTPUT -j MARK_PACKETS
-sudo iptables -t mangle -A MARK_PACKETS -d $COOLTEE_MAL_REQ_SUBNET -j RETURN
-sudo iptables -t mangle -A MARK_PACKETS -j MARK --set-mark 46
+sudo iptables -t mangle -A MARK_PACKETS -d $COOLTEE_HON_REQ_SUBNET -j MARK --set-mark 46
 
 sudo tc qdisc add dev eth0 root handle 1: prio
 sudo tc qdisc add dev eth0 parent 1:1 handle 10: netem delay $ATTACK_DELAY
 sudo tc filter add dev eth0 protocol ip parent 1:0 prio 1 handle 46 fw flowid 1:1
-echo -e "${PINK}Enabled the malicious delay attack.${NC}"
+echo "${PINK}Enabled the malicious delay attack.${NC}"
