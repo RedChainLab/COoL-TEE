a47785ece2f7e353242b69a425fa27ef09b303e7
mbettinger <49953678+mbettinger@users.noreply.github.com>
2024-12-02 23:22:00 +0100

    Cuckoo attack environment configurator

---
+++ sim/search/src/CuckooEnv.h
@@ -0,0 +1,35 @@
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/.
+// 
+
+#ifndef __COOL_TEE_CUCKOOENV_H_
+#define __COOL_TEE_CUCKOOENV_H_
+
+#include <omnetpp.h>
+
+#include "HardCodedEnv.h"
+
+using namespace omnetpp;
+
+/**
+ * TODO - Generated class
+ */
+class CuckooEnv : public HardCodedEnv
+{
+  protected:
+    virtual void initialize(int stage) override;
+    virtual void assignLoads();
+};
+
+#endif
